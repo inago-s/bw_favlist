10ff56aa3ab7f70f9911690ae65badee261d89d8
inago-s <inago5173@gmail.com>
2021-09-29 01:17:47 +0900

    [change]配信開始日と雑誌フラグの取得

--- js/get_book_info.js
+++ js/get_book_info.js
@@ -1,5 +1,7 @@
 data = {
-	limit: $('.main-limit :contains("読み放題期限")').next().text(),
+	release: $('.work-detail :contains("配信開始日")').next().text().trim(),
+	limit: $('.main-limit :contains("読み放題期限")').next().text().trim(),
 	series: $('.work-detail :contains("シリーズ")').next().text().trim(),
-	latest: $('.series .cmnShelf-info :contains("読み放題")').parent(':first').children('.cmnShelf-head').text()
+	latest: $('.series .cmnShelf-info :contains("読み放題")').parent(':first').children('.cmnShelf-head').text(),
+	magazine_flag: $('.work-tag-item').text().includes('雑誌')
 }
